3afb9cf9ceeb3c80fe9879f4b268bc6f7123a409
Alejandro Lopez <alejandrolopez@MacBook-Pro-4.local>
2022-07-26 17:22:24 -0400

    make it better

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routesHandler = require("./routes/handler.js");
+const cors = require("cors");
 
 require("dotenv/config");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.listen(4001, function () {
   console.log("Running... ");
